1d475b937acc004a592baa871583472ba5e159a7
moha <khabbazian@wisc.edu>
2015-10-09 11:24:00 -0500

    no.margin plot.phylo

--- R/tools.R
+++ R/tools.R
@@ -197,7 +197,6 @@ plot_l1ou <- function(tree, model, pallet=NA, ...){
     Y = as.matrix(model$Y);
     stopifnot(identical(rownames(Y), tree$tip.label));
 
-    par(oma=c(3,2,0,0))
     layout(matrix(1:(1+ncol(Y)), 1, (1+ncol(Y))), width=c(2,1,1,1,1));
 
     if(is.na(pallet)){
@@ -230,6 +229,7 @@ plot_l1ou <- function(tree, model, pallet=NA, ...){
         barcol[[i]]  = edgecol[  which( tree$edge[,2] == i)  ];
     }
 
+    par(mar=c(3,3,0,0))
     for(i in 1:ncol(Y)){
         normy = (Y[,i] - mean(Y[,i]))/sd(Y[,i]);
         barplot (as.vector(normy), border=FALSE, col=barcol, horiz = TRUE, names.arg = "", xaxt = "n");
