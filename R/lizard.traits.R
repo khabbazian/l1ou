6e2bc071da4eb1268a8e21608ce480c8cccf7026
moha <khabbazian@wisc.edu>
2015-10-17 16:00:01 -0500

    added doc for datasets

--- R/lizard.traits.R
+++ R/lizard.traits.R
@@ -1,6 +1,17 @@
-# 
-#' Lizard traits
+#' Morphospace of 100 Anolis lizards on Caribbean islands
+#'
+#'@docType data
+#'
+#'@name lizard.traits
+#'
+#'@format a matrix with 100 rows and 4 columns
 #'
 #'@usage
 #'data(lizard.traits)
 #'
+#'@keywords datasets
+#'
+#'@references
+#'Mahler, D. Luke, et al. "Exceptional convergence on the macroevolutionary landscape in island lizard radiations." Science 341.6143 (2013): 292-295.'
+#'
+NULL
--- R/lizard.tree.R
+++ R/lizard.tree.R
@@ -1,5 +1,17 @@
-# 
-#' Lizard phylogenetic tree 
+#'Phylogenetic tree of 100 Anolis lizards on Caribbean islands 
+#'
+#'@docType data
+#'
+#'@name lizard.tree
+#'
+#'@format a phylogenetic tree of class phylo with 100 species.
 #'
 #'@usage
 #'data(lizard.tree)
+#'
+#'@keywords datasets
+#'
+#'@references
+#'Mahler, D. Luke, et al. "Exceptional convergence on the macroevolutionary landscape in island lizard radiations." Science 341.6143 (2013): 292-295.'
+#'
+NULL
