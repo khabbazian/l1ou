252be1f7a2762c11b9fb2e632221ba68b2cba660
khabbazian <khabbazian@wisc.edu>
2015-11-22 17:41:40 -0600

    minor changes

--- src/sqrt_OU_covariance.cpp
+++ src/sqrt_OU_covariance.cpp
@@ -17,7 +17,7 @@ void one_step(const int i1, const int i2, const int e1, const int e2,
 
     const int nEdges = edgeList.nrow();
 
-    int e1=-1, e2=-1;
+    //int e1=-1, e2=-1;
     for (int i=0; i<nEdges; ++i)
         if (edgeList(i,1) == i1)
             e1 = i;
